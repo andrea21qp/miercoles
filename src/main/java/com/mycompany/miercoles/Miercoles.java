41995f03fda644e8616ddfbeb7f272cd2f8275db
unknown <andreaquiroga010@gmail.com>
2025-04-22 12:52:46 -0500

    lunes

---
+++ src/main/java/com/mycompany/miercoles/Miercoles.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miercoles;
+
+/**
+ *
+ * @author DELL
+ */
+public class Miercoles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Andrea!");
+    }
+}
